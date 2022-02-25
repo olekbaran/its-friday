ae5a72f9aa08910224e6d53feeac7b7ae79be037
olek-arsee <olboygg@gmail.com>
2022-02-25 22:07:40 +0100

    send embed every friday

--- bot/bot.go
+++ bot/bot.go
@@ -3,8 +3,11 @@ package bot
 import (
 	"fmt"
 	"its-friday/config"
+	"net/http"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
+	"gopkg.in/robfig/cron.v2"
 )
 
 var BotId string
@@ -35,6 +38,30 @@ func Start() {
 		return
 	}
 	fmt.Println("Bot is running !")
+
+	c := cron.New()
+	c.AddFunc("0 6 * * FRI", friday)
+	c.Start()
+}
+
+func friday() {
+	f, err := os.Open("friday-message.json")
+	if err != nil {
+		fmt.Println("Error with friday-message.json")
+	}
+	defer f.Close()
+	req, err := http.NewRequest("POST", os.ExpandEnv("https://discord.com/api/channels/774597138318360586/messages"), f)
+	if err != nil {
+		fmt.Println("Error with sending a request")
+	}
+	req.Header.Set("Authorization", os.ExpandEnv("Bot "+config.Token))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Error with response")
+	}
+	defer resp.Body.Close()
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module its-friday
 
 go 1.17
 
-require github.com/bwmarrin/discordgo v0.23.2
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	gopkg.in/robfig/cron.v2 v2.0.0-20150107220207-be2e0b0deed5
+)
 
 require (
 	github.com/gorilla/websocket v1.4.0 // indirect
