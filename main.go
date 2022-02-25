01a97b8a6effd274407634b6321763394cd260f6
olek-arsee <olboygg@gmail.com>
2022-02-25 15:40:42 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"its-friday/bot"
+	"its-friday/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
