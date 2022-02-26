a2a3d1d3fc8fc54a7f9088a3c02f490c76e33850
olek-arsee <olboygg@gmail.com>
2022-02-26 21:54:21 +0100

    delete message on monday

--- bot/bot.go
+++ bot/bot.go
@@ -1,16 +1,24 @@
 package bot
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"its-friday/config"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"gopkg.in/robfig/cron.v2"
 )
 
 var BotId string
+var fridayMessageId string
+
+type fridayMessageResponse struct {
+	Id string
+}
 
 func Start() {
 	goBot, err := discordgo.New("Bot " + config.Token)
@@ -37,11 +45,15 @@ func Start() {
 		fmt.Println(err.Error())
 		return
 	}
-	fmt.Println("Bot is running !")
+	fmt.Println("Bot is running!")
 
-	c := cron.New()
-	c.AddFunc("* * * * *", friday)
-	c.Start()
+	fridayCron := cron.New()
+	fridayCron.AddFunc("0 6 * * FRI", friday)
+	fridayCron.Start()
+
+	mondayCron := cron.New()
+	mondayCron.AddFunc("0 0 * * MON", monday)
+	mondayCron.Start()
 }
 
 func friday() {
@@ -59,9 +71,47 @@ func friday() {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println("Error with response")
+		fmt.Println("Error with a response")
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Println("Error with a response")
+		}
+
+		var fridayMessageResponse1 fridayMessageResponse
+		errResponse := json.Unmarshal(bodyBytes, &fridayMessageResponse1)
+		if err != nil {
+			fmt.Println(errResponse)
+		}
+
+		dateFriday := time.Now()
+		fmt.Println("-----LOGS-----")
+		fmt.Println(dateFriday.Format("01-02-2006") + " Create: " + fridayMessageResponse1.Id)
+		fridayMessageId = fridayMessageResponse1.Id
+	}
+}
+
+func monday() {
+	req, err := http.NewRequest("DELETE", "https://discord.com/api/channels/"+config.FridayMessageChannel+"/messages/"+fridayMessageId, nil)
+	if err != nil {
+		fmt.Println("Error with sending a request")
+	}
+	req.Header.Set("Authorization", "Bot "+config.Token)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Error with a response")
 	}
 	defer resp.Body.Close()
+
+	dateMonday := time.Now()
+	fmt.Println(dateMonday.Format("01-02-2006") + " Delete: " + fridayMessageId)
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
